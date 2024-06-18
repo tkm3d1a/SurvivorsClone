ff3eab354658b329900021bea73659040f7ddabd
Tim Klimpel <tim.klimpel@gmail.com>
2024-06-18 14:43:45 -0400

    Added base player and level - no tutorial started yet

---
+++ Survivors Clone/Scripts/Levels/Level01/level_01.gd
@@ -0,0 +1,5 @@
+class_name level_01
+extends Node2D
+
+func _ready() -> void:
+    printerr("Script not complete: " + name)
---
+++ Survivors Clone/Scripts/Player/player.gd
@@ -0,0 +1,5 @@
+class_name player
+extends CharacterBody2D
+
+func _ready() -> void:
+    printerr("Script not complete: " + name)
