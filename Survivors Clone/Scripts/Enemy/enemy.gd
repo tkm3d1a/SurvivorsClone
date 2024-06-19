51066c57aa9436624b209779e1b05d1c7d6f7119
Tim Klimpel <tim.klimpel@gmail.com>
2024-06-18 23:28:05 -0400

    Adding animations to player and enemy

--- Survivors Clone/Scripts/Enemy/enemy.gd
+++ Survivors Clone/Scripts/Enemy/enemy.gd
@@ -1,12 +1,17 @@
 class_name enemy
 # HACK: possible issue with class name not matching scene name in the future with other enemy types?
+# TODO: clean up and inherit from a base 'character' class
 extends CharacterBody2D
 
-@export var move_speed: float = 30.0
+@export var move_speed: float = 30.0 # common
 
 @onready var target: player = get_tree().get_first_node_in_group("player")
+@onready var sprite_node: Sprite2D = $Sprite2D # common
+@onready var anim_player_node: AnimationPlayer = get_node("AnimationPlayer")
 
 func _ready() -> void:
+    anim_player_node.play("walk")
+
     # TODO: Remove when script complete for mvp
     printerr("Script not complete: " + name)
 
@@ -14,4 +19,12 @@ func _physics_process(_delta: float) -> void:
     # TODO: Create function for movement
     var direction_to_player: Vector2 = global_position.direction_to(target.global_position)
     velocity = direction_to_player * move_speed
+    flip_sprite(direction_to_player.x)
+
     move_and_slide()
+
+func flip_sprite(move_x_result: float) -> void: # common
+    if move_x_result > 0:
+        sprite_node.flip_h = true
+    elif move_x_result < 0:
+        sprite_node.flip_h = false
--- Survivors Clone/Scripts/Player/player.gd
+++ Survivors Clone/Scripts/Player/player.gd
@@ -1,7 +1,11 @@
 class_name player
+# TODO: clean up and inherit from a base 'character' class
 extends CharacterBody2D
 
-@export var move_speed: float = 75.0
+@export var move_speed: float = 75.0 #common
+
+@onready var sprite_node: Sprite2D = $Sprite2D #common
+@onready var walk_timer_node: Timer = get_node("WalkTimer")
 
 func _ready() -> void:
     # TODO: Remove when script complete for mvp
@@ -15,7 +19,23 @@ func movement() -> void:
     var x_move_input: float = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
     var y_move_input: float = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
     var move_input: Vector2 = Vector2(x_move_input, y_move_input)
+    flip_sprite(move_input.x)
+    animate_sprite(move_input)
 
     velocity = move_input.normalized() * move_speed
-
     move_and_slide()
+
+func flip_sprite(move_x_result: float) -> void: #common
+    if move_x_result > 0:
+        sprite_node.flip_h = true
+    elif move_x_result < 0:
+        sprite_node.flip_h = false
+
+func animate_sprite(move_result: Vector2) -> void:
+    if move_result != Vector2.ZERO:
+        if walk_timer_node.is_stopped():
+            if sprite_node.frame >= sprite_node.hframes - 1:
+                sprite_node.frame = 0
+            else:
+                sprite_node.frame += 1
+            walk_timer_node.start()
