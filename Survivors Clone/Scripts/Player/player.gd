b6c4ffa235700151ca3d22f426a5c53f1af0cb39
Tim Klimpel <tim.klimpel@gmail.com>
2024-06-18 22:45:46 -0400

    Part 1 complete

--- Survivors Clone/Scripts/Player/player.gd
+++ Survivors Clone/Scripts/Player/player.gd
@@ -1,5 +1,20 @@
 class_name player
 extends CharacterBody2D
 
+@export var move_speed: float = 75.0
+
 func _ready() -> void:
     printerr("Script not complete: " + name)
+
+func _physics_process(_delta: float) -> void:
+    movement()
+
+func movement() -> void:
+    # TODO: Update to implement globals for inputs
+    var x_move_input: float = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
+    var y_move_input: float = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
+    var move_input: Vector2 = Vector2(x_move_input, y_move_input)
+
+    velocity = move_input.normalized() * move_speed
+
+    move_and_slide()
